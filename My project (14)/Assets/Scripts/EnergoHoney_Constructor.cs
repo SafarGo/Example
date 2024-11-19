f23a1c8d6a23d6de2632561fe1bf97e8e595ef30
HardGrin <greenelekt@gmail.com>
2024-11-19 03:23:34 +0300

    FiniFinish_Night_Commit

--- My project (14)/Assets/Scripts/EnergoHoney_Constructor.cs
+++ My project (14)/Assets/Scripts/EnergoHoney_Constructor.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class HoneySpawner : MonoBehaviour
 {
-    public GameObject energyHoneyPrefab; 
-
+    public GameObject energyHoneyPrefab;
+    [SerializeField] int HoneyDelSpeed;
+    int medCaunter;
+    [SerializeField] int cartridgeCount;
     private void OnTriggerEnter(Collider other)
     {
 
@@ -12,14 +15,20 @@ public class HoneySpawner : MonoBehaviour
 
             Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
             Vector3 velocity = otherRigidbody != null ? otherRigidbody.velocity : Vector3.zero;
-            SpawnEnergyHoney(other.transform.position, velocity);
             Destroy(other.gameObject);
+            medCaunter++;
+                if (medCaunter == 2)
+            {
+                medCaunter = 0;
+                StartCoroutine(SpawnEnergyHoney(other.transform.position, velocity));
+            }
         }
     }
 
-    private void SpawnEnergyHoney(Vector3 position, Vector3 velocity)
+    private IEnumerator SpawnEnergyHoney(Vector3 position, Vector3 velocity)
     {
-   
+
+        yield return new WaitForSeconds(HoneyDelSpeed / cartridgeCount);
         GameObject newEnergyHoney = Instantiate(energyHoneyPrefab, position, Quaternion.identity);
 
  
