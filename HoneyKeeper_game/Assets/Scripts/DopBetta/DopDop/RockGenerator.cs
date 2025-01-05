3df9e477088c192b9beab0254d658ab409cdb0fa
HardGrin <greenelekt@gmail.com>
2025-01-05 12:42:25 +0300

    12_42_Sasha_Commit

--- HoneyKeeper_game/Assets/Scripts/DopBetta/DopDop/RockGenerator.cs
+++ HoneyKeeper_game/Assets/Scripts/DopBetta/DopDop/RockGenerator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class RockGenerator : MonoBehaviour
 {
     [Header("Настройки деформации")]
